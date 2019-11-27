1af4b41f82cc3d76c384b7e1aa4b576766f7a178
Michał Wanta <michal.wanta@euco.pl>
2019-11-27 17:59:51 +0100

    new project

---
+++ README.md
@@ -0,0 +1,21 @@
+# Your-life-site
+40. Individual project called "Just od it"
+
+# repo -> https://github.com/MichalAW/Your-life-site.git
+
+# link -> 
+
+# Script
+1. Download repository
+2. Launch script "watch" to visit site
+
+# Package.json scripts
+1. "prewatch" - clean dist
+2. "watch" - run site
+3. "sass" - convert scss to css
+4. "prebuild" - clean dist for build
+5. "build" - build package for heroku
+6. "clean" - run rimraf
+7. "code-quality" - check code
+8. "format" - add preatier
+# Design bought
---
+++ src/scripts/script.js
@@ -0,0 +1,30 @@
+var links = document.querySelectorAll(".topbar a");
+var sections = document.querySelectorAll("section");
+
+function toggleTopbar() {
+	document.getElementById("topbar").classList.toggle("active");
+}
+
+// event for all links in topbar
+for (var i = 0; i < links.length; i++) {
+	links[i].addEventListener("click", function() {
+		// remove class active from all sections
+		for (var i = 0; i < sections.length; i++) {
+			sections[i].classList.remove("active");
+		}
+	});
+
+	toggle.addEventListener("click", function() {
+		toggleTopbar();
+	});
+}
+
+// validation of the form
+document.querySelector(".btn-send").addEventListener("click", function() {
+	var textareaVal = document.querySelector(".contact textarea").value;
+	if (textareaVal.length > 0) {
+		return;
+	} else {
+		alert("Your message field is required!");
+	}
+});
